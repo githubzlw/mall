313e2be3e71b0d789430fabe6ac23336e36b5353
jack.luo <luohao518@yeah.net>
2021-04-16 14:26:35 +0800

    版本修改

--- mall-portal/src/main/java/com/macro/mall/portal/component/OrderTimeOutCancelTask.java
+++ mall-portal/src/main/java/com/macro/mall/portal/component/OrderTimeOutCancelTask.java
@@ -5,13 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by macro on 2018/8/24.
  * 订单超时取消并解锁库存的定时器
  */
-@Component
+//@Component
 public class OrderTimeOutCancelTask {
     private Logger LOGGER =LoggerFactory.getLogger(OrderTimeOutCancelTask.class);
     @Autowired
