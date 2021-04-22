b6dda9cf4d76b1779d655486c92956631b6b06d3
jack.luo <luohao518@yeah.net>
2021-04-22 14:43:47 +0800

    add new two table

---
+++ mall-portal/src/main/java/com/macro/mall/portal/service/IXmsMsgService.java
@@ -0,0 +1,16 @@
+package com.macro.mall.portal.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.macro.mall.entity.XmsMsg;
+
+/**
+ * <p>
+ * 公告表 服务类
+ * </p>
+ *
+ * @author jack.luo
+ * @since 2021-04-22
+ */
+public interface IXmsMsgService extends IService<XmsMsg> {
+
+}
---
+++ mall-portal/src/main/java/com/macro/mall/portal/service/impl/XmsMsgrecycleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.macro.mall.portal.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.macro.mall.entity.XmsMsgrecycle;
+import com.macro.mall.mapper.XmsMsgrecycleMapper;
+import com.macro.mall.portal.service.IXmsMsgrecycleService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author jack.luo
+ * @since 2021-04-22
+ */
+@Service
+public class XmsMsgrecycleServiceImpl extends ServiceImpl<XmsMsgrecycleMapper, XmsMsgrecycle> implements IXmsMsgrecycleService {
+
+}
