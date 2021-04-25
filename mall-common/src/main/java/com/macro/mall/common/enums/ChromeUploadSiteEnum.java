fd299587a503221eb4f4faf471f1b367b6639ae9
jack.luo <luohao518@yeah.net>
2021-04-25 10:11:28 +0800

    add new site:https://www.1688.com/

--- mall-common/src/main/java/com/macro/mall/common/enums/ChromeUploadSiteEnum.java
+++ mall-common/src/main/java/com/macro/mall/common/enums/ChromeUploadSiteEnum.java
@@ -10,7 +10,7 @@ public enum ChromeUploadSiteEnum {
     ALIBABA(1,"https://www.alibaba.com/"), ALIEXPRESS(2,"https://www.aliexpress.com/")
     , ESALIEXPRESS(3,"https://es.aliexpress.com/"), AMAZON(4,"https://www.amazon.cn/")
     , WAYFAIR(5,"https://www.wayfair.com/"), EBAY(6,"https://www.ebay.com/")
-    , WALMART(7,"https://www.walmart.com/");
+    , WALMART(7,"https://www.walmart.com/"),ALI1688(8,"https://www.1688.com/");
 
     private int siteType;
     private String siteDomain;
