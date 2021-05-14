e7038526e07a9ad3ad59d2ab03f87630cc0eb9b8
jinjie <jinjie19840221@sina.com>
2021-05-14 10:40:39 +0800

    获取用户信息的接口。

--- mall-portal/src/main/java/com/macro/mall/portal/service/UmsMemberService.java
+++ mall-portal/src/main/java/com/macro/mall/portal/service/UmsMemberService.java
@@ -61,4 +61,14 @@ public interface UmsMemberService {
      * 刷新token
      */
     String refreshToken(String token);
+
+
+    /**
+     *更新客户的 shopify信息
+     * @param id
+     * @param shopifyName
+     * @param shopifyFlag
+     * @return
+     */
+    int updateShopifyInfo(Long id, String shopifyName,Integer shopifyFlag);
 }
