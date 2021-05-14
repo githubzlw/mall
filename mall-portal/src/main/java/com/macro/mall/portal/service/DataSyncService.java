cba62c2a7eba0927c031e7cb424b1846fa3d3430
jinjie <jinjie19840221@sina.com>
2021-05-14 10:19:18 +0800

    获取用户信息的接口。

---
+++ mall-portal/src/main/java/com/macro/mall/portal/service/DataSyncService.java
@@ -0,0 +1,20 @@
+package com.macro.mall.portal.service;
+
+import com.macro.mall.model.UmsMember;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 数据同步Service
+ * Created by jinjie on 2021/5/14.
+ */
+public interface DataSyncService {
+
+    /**
+     * 获取所有的用户信息
+     */
+    List<UmsMember> getAllUser(Long id);
+
+}
---
+++ mall-portal/src/main/java/com/macro/mall/portal/service/impl/DataSyncServiceImpl.java
@@ -0,0 +1,60 @@
+package com.macro.mall.portal.service.impl;
+
+import com.macro.mall.common.exception.Asserts;
+import com.macro.mall.mapper.UmsMemberLevelMapper;
+import com.macro.mall.mapper.UmsMemberMapper;
+import com.macro.mall.model.UmsMember;
+import com.macro.mall.model.UmsMemberExample;
+import com.macro.mall.model.UmsMemberLevel;
+import com.macro.mall.model.UmsMemberLevelExample;
+import com.macro.mall.portal.domain.MemberDetails;
+import com.macro.mall.portal.service.DataSyncService;
+import com.macro.mall.portal.service.UmsMemberCacheService;
+import com.macro.mall.portal.service.UmsMemberService;
+import com.macro.mall.security.util.JwtTokenUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 会员管理Service实现类
+ * Created by macro on 2018/8/3.
+ */
+@Service
+public class DataSyncServiceImpl implements DataSyncService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataSyncServiceImpl.class);
+
+    @Autowired
+    private UmsMemberMapper memberMapper;
+
+    @Override
+    public List<UmsMember> getAllUser(Long id) {
+
+        UmsMemberExample example = new UmsMemberExample();
+        if (id != null && id > 0) {
+            example.createCriteria().andIdEqualTo(id);
+        }
+
+        List<UmsMember> memberList = memberMapper.selectByExample(example);
+
+        return memberList;
+    }
+
+}
