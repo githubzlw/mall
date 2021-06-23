dca9ff4f3b182a095c7e45649264264af99f1a11
zlw <117007671@qq.com>
2021-06-23 13:17:20 +0800

    第三方登录修改

--- mall-portal/src/main/java/com/macro/mall/portal/service/UmsMemberService.java
+++ mall-portal/src/main/java/com/macro/mall/portal/service/UmsMemberService.java
@@ -97,6 +97,11 @@ public interface UmsMemberService {
     ImmutablePair<String, String> googleAuth(String idTokenString) throws IOException;
 
     FacebookPojo facebookAuth(String code);
+    /**
+     * get facebook login url
+     * @return
+     */
+    String getFacebookUrl();
 
     int updateGuidedFlag(Long id);
 
