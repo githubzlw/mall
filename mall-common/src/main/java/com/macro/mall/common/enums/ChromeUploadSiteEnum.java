3f8571c1bb339f047ba9fe9e3a65d7b2acc3ff76
jiangxianwei <1071083166@qq.com>
2021-07-22 14:17:11 +0800

    插件添加支持alibaba和速卖通国际站的网站

--- mall-common/src/main/java/com/macro/mall/common/enums/ChromeUploadSiteEnum.java
+++ mall-common/src/main/java/com/macro/mall/common/enums/ChromeUploadSiteEnum.java
@@ -7,17 +7,23 @@ import lombok.Data;
  */
 public enum ChromeUploadSiteEnum {
 
-    ALIBABA(1,"https://www.alibaba.com/"), ALIEXPRESS(2,"https://www.aliexpress.com/")
-    , ESALIEXPRESS(3,"https://es.aliexpress.com/"), AMAZON(4,"https://www.amazon.com/")
-    , WAYFAIR(5,"https://www.wayfair.com/"), EBAY(6,"https://www.ebay.com/")
-    , WALMART(7,"https://www.walmart.com/"),ALI1688(8,"https://detail.1688.com/");
+    ALIBABA(1,"https://www.alibaba.com/","alibaba"),
+    ALIEXPRESS(2,"https://www.aliexpress.com/","aliexpress"),
+    // ESALIEXPRESS(3,"https://es.aliexpress.com/","aliexpress"),
+    AMAZON(4,"https://www.amazon.com/","amazon"),
+    WAYFAIR(5,"https://www.wayfair.com/","wayfair"),
+    EBAY(6,"https://www.ebay.com/","ebay"),
+    WALMART(7,"https://www.walmart.com/","walmart"),
+    ALI1688(8,"https://detail.1688.com/","detail");
 
     private int siteType;
     private String siteDomain;
+    private String siteName;
 
-    ChromeUploadSiteEnum(int siteType,String siteDomain){
+    ChromeUploadSiteEnum(int siteType,String siteDomain, String siteName){
         this.siteType = siteType;
         this.siteDomain = siteDomain;
+        this.siteName = siteName;
     }
 
     public int getSiteType(){
@@ -26,5 +32,9 @@ public enum ChromeUploadSiteEnum {
 
     public String getSiteDomain(){
         return this.siteDomain;
+    }
+
+    public String getSiteName() {
+        return siteName;
     }
 }
