d13f2248630147d5edeca89a2f0d71628915ad2e
jiangxianwei <1071083166@qq.com>
2021-07-23 13:37:40 +0800

    BFM支持alibaba和aliexpress国际站

--- mall-portal/src/main/java/com/macro/mall/portal/util/SiteFlagEnum.java
+++ mall-portal/src/main/java/com/macro/mall/portal/util/SiteFlagEnum.java
@@ -7,23 +7,24 @@ package com.macro.mall.portal.util;
  * @date:2021-04-01
  */
 public enum SiteFlagEnum {
-    ALIBABA("https://www.alibaba.com", 1, "88800001"),
-    ALIEXPRESS("https://www.aliexpress.com", 2, "88800002"),
-    ESALIEXPRESS("https://es.aliexpress.com", 3, "88800003"),
-    AMAZON("https://www.amazon.cn", 4, "88800005"),
-    WAYFAIR("https://www.wayfair.com", 5, "88800005"),
-    EBAY("https://www.ebay.com", 6, "88800006"),
-    WALMART("https://www.walmart.com", 7, "88800007"),
-    ALI1688("https://detail.1688.com", 8, "88800008"),
+    ALIBABA("https://www.alibaba.com", 1, "88800001", "alibaba"),
+    ALIEXPRESS("https://www.aliexpress.com", 2, "88800002", "aliexpress"),
+    //ESALIEXPRESS("https://es.aliexpress.com", 3, "88800003"),
+    AMAZON("https://www.amazon.cn", 4, "88800005", "amazon"),
+    WAYFAIR("https://www.wayfair.com", 5, "88800005", "wayfair"),
+    EBAY("https://www.ebay.com", 6, "88800006", "ebay"),
+    WALMART("https://www.walmart.com", 7, "88800007", "walmart"),
+    ALI1688("https://detail.1688.com", 8, "88800008", "detail1688"),
 
-    IMG_ONLY("imgUrl", 9, "88800009"),
-    OTHER("otherUrl", 10, "88800010"),
-    SHOPIFY("shopifyUrl", 11, "888000011");
+    IMG_ONLY("imgUrl", 9, "88800009", "img_only"),
+    OTHER("otherUrl", 10, "88800010", "other_url"),
+    SHOPIFY("shopifyUrl", 11, "888000011", "shopify_url");
 
 
     private String url;
     private int flag;
     private String catid;
+    private String name;
 
     public String getUrl() {
         return url;
@@ -37,9 +38,14 @@ public enum SiteFlagEnum {
         return catid;
     }
 
-    SiteFlagEnum(String url, int flag, String catid) {
+    public String getName() {
+        return name;
+    }
+
+    SiteFlagEnum(String url, int flag, String catid, String name) {
         this.url = url;
         this.flag = flag;
         this.catid = catid;
+        this.name = name;
     }
 }
