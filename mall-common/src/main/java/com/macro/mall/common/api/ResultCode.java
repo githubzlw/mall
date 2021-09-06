82491464d39dc2fb75ec9a5c425da4077f5e1147
jiangxianwei <1071083166@qq.com>
2021-09-06 16:23:20 +0800

    字段中文处理

--- mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
+++ mall-common/src/main/java/com/macro/mall/common/api/ResultCode.java
@@ -5,11 +5,11 @@ package com.macro.mall.common.api;
  * Created by macro on 2019/4/19.
  */
 public enum ResultCode implements IErrorCode {
-    SUCCESS(200, "操作成功"),
-    FAILED(500, "操作失败"),
-    VALIDATE_FAILED(404, "参数检验失败"),
-    UNAUTHORIZED(401, "暂未登录或token已经过期"),
-    FORBIDDEN(403, "没有相关权限");
+    SUCCESS(200, "Operation is successful"),
+    FAILED(500, "The operation failure"),
+    VALIDATE_FAILED(404, "Parameter verification failure"),
+    UNAUTHORIZED(401, "No login is performed or the token has expired"),
+    FORBIDDEN(403, "No relevant permissions");
     private long code;
     private String message;
 
